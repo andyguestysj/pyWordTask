da18ed2896fe8c9df47bf7f0b43dbc585f6698dd
andyguest <59604680+andyguestysj@users.noreply.github.com>
2021-10-11 18:43:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# 1 Create a function called process_pigs
+# 2 Create an empty list called words_list
+# 2 In process_pigs open the file "littlepigs.txt" for reading*
+# 3 Use a "for in" loop to read each line from the file* 
+# 4 Get a list of words in for each line
+# 5 Add each word to your words_list
+# 6 When you've finished reading the file sort words_list
+# 7 Print out the words_list
+
+# 8 Bonus Task 1 - From words_list create unique_words_list that contains each word only once
+# 9 Bonus Task 2 - Modify your code from #8 so that each entry in unique_words_list consists of a list of a word and the count of that word. eg. [['a',50],['bat',1],['brick',30]...] (numbers not correct in example)
+
+# * The links on the labs page will hel1 here
+
+
+
+
+def main():
+
+  process_pigs()
+
+
+
+
+if __name__ == "__main__":
+  main()
+
